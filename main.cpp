57b2e7142980d9e8c5381007ad6d7070fc82d205
julianrtsonchev <juliantsonchev@abv.bg>
2026-05-17 12:29:37 +0300

    adding food library a class that has food products

--- main.cpp
+++ main.cpp
@@ -2,39 +2,47 @@
 #include "user.h"
 #include "FoodProduct.h"
 #include "Meal.h"
+#include "foodlibrary.h"
 using namespace std;
 
 int main() {
-    // try {
-    //     User user("Ivan Ivanov", 'M', 180, 75, 30, 4, "Gain");
-    //     user.displayProfile();
-    // } catch (const invalid_argument& e) {
-    //     cout << "Error: " << e.what() << endl;
-    // }
-    
-    // try {
-    //     User invalidUser("Sigma", 's', 170, 0, -5, 10, "Invalid Goal");
-    // } catch (const invalid_argument& e) {
-    //     cout << "Error: " << e.what() << endl;
-    // }
-    
+    User juli("Yuliyan Tsonchev", 'M', 173, 63, 16, 3, "Gain");
+    FoodLibrary library;
+
     try {
-        User juli("Yuliyan Tsonchev", 'M', 173, 63, 16, 3, "Gain");
         juli.displayProfile();
     } catch (const invalid_argument& e) {
-        cout << "Error: " << e.what() << endl;
+        cout << "Error displaying profile: " << e.what() << endl;
     }
-    try{
-        FoodProduct apple("Apple", 0.3, 14, 0.2);
-        FoodProduct bread("Bread", 8, 50, 1.5);
-        Meal breakfast("Breakfast");
-        breakfast.addProduct(apple, 150);
-        breakfast.addProduct(bread, 100);
-        breakfast.displayMeal();
-        Meal lunch("Lunch");
-        lunch.displayMeal();
+
+    try {
+        library.createProduct(FoodProduct("Apple", 0.3, 14, 0.2));
+        library.createProduct(FoodProduct("Bread", 8, 50, 1.5));
+        library.createProduct(FoodProduct("Chicken Breast", 31, 0, 3.6));
+        library.createProduct(FoodProduct("Rice", 2.7, 28, 0.3));
+        library.createProduct(FoodProduct("Broccoli", 2.8, 7, 0.4));
+        
+        library.updateProduct("Bread", 9, 48, 2);
+        library.deleteProduct("Rice");
+        
+        library.displayLibrary();
     } catch (const invalid_argument& e) {
-        cout << "Error: " << e.what() << endl;
+        cout << "Library Error: " << e.what() << endl;
     }
+
+    try {
+        Meal breakfast("Breakfast");
+        breakfast.addProduct(library.readProduct("Apple"), 150);
+        breakfast.addProduct(library.readProduct("Bread"), 100);
+        breakfast.displayMeal();
+
+        Meal lunch("Lunch");
+        lunch.addProduct(library.readProduct("Chicken Breast"), 150);   
+        lunch.displayMeal();
+        
+    } catch (const invalid_argument& e) {
+        cout << "Meal Tracker Error: " << e.what() << endl;
+    }
+
     return 0;
 }
